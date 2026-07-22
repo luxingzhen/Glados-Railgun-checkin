8192efd5e192b0500be141e6c36b56c9bc3de79b
luxingzhen <38696937+luxingzhen@users.noreply.github.com>
2026-07-22 15:47:55 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- checkin.py
+++ checkin.py
@@ -106,8 +106,7 @@ class Config:
     DEFAULT_VERBOSE = False
 
     """默认域名"""
-    DOMAINS = ["glados.cloud", "railgun.info"]
-
+    DOMAINS = ["glados.cloud"]
     """兑换计划列表"""
     EXCHANGE_PLANS = {
         ExchangePlan.PLAN100.value: 100,
